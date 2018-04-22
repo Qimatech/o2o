ea2936ccd644eff9d95bc5eacab1d8a78005aeed
qimainfo <m18721632613_1@163.com>
2018-04-23 06:05:29 +0800

    init

--- src/main/java/com/qima/o2o/web/MvcConfiguration.java
+++ src/main/java/com/qima/o2o/web/MvcConfiguration.java
@@ -1,6 +1,10 @@
 package com.qima.o2o.web;
 
+import javax.servlet.ServletException;
+
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -13,46 +17,82 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.google.code.kaptcha.servlet.KaptchaServlet;
+
 @Configuration
-//等价于<mvc:annotation-driven/>
+// 等价于<mvc:annotation-driven/>
 @EnableWebMvc
 public class MvcConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
 	private ApplicationContext applicationContext;
-	
+
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
 	}
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		//registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/");
+		// registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/");
 	}
-	
+
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
-	
-	@Bean(name="viewResolver")
+
+	@Bean(name = "viewResolver")
 	public ViewResolver createViewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		
+
 		viewResolver.setApplicationContext(this.applicationContext);
 		viewResolver.setCache(false);
 		viewResolver.setPrefix("/WEB-INF/html/");
 		viewResolver.setSuffix(".html");
-		
+
 		return viewResolver;
 	}
-	
+
 	public CommonsMultipartResolver createMultipartResolver() {
 		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
 		multipartResolver.setDefaultEncoding("utf-8");
 		multipartResolver.setMaxUploadSize(20971520);
 		multipartResolver.setMaxInMemorySize(20971520);
 		return multipartResolver;
+	}
+
+	@Value("${kaptcha.border}")
+	private String border;
+	@Value("${kaptcha.textproducer.font.color}")
+	private String fcolor;
+	@Value("${kaptcha.image.width}")
+	private String width;
+	@Value("${kaptcha.textproducer.char.string}")
+	private String cString;
+	@Value("${kaptcha.image.height}")
+	private String height;
+	@Value("${kaptcha.textproducer.font.size}")
+	private String fsize;
+	@Value("${kaptcha.noise.color}")
+	private String nColor;
+	@Value("${kaptcha.textproducer.char.length}")
+	private String clenth;
+	@Value("${kaptcha.textproducer.font.names}")
+	private String fnames;
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() throws ServletException {
+		ServletRegistrationBean servlet = new ServletRegistrationBean(new KaptchaServlet(), "/Kaptcha");
+		servlet.addInitParameter("kaptcha.border", border);
+		servlet.addInitParameter("kaptcha.textproducer.font.color", fcolor);
+		servlet.addInitParameter("kaptcha.image.width", width);
+		servlet.addInitParameter("kaptcha.textproducer.char.string", cString);
+		servlet.addInitParameter("kaptcha.image.height", height);
+		servlet.addInitParameter("kaptcha.textproducer.font.size", fsize);
+		servlet.addInitParameter("kaptcha.noise.color", nColor);
+		servlet.addInitParameter("kaptcha.textproducer.char.length", clenth);
+		servlet.addInitParameter("kaptcha.textproducer.font.names", fnames);
+		return servlet;
 	}
 
 }
