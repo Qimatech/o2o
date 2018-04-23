af093ed8ff8c1f7a20583f56eb2402a9866a3f78
qimainfo <m18721632613_1@163.com>
2018-04-24 07:36:34 +0800

    init

--- src/main/java/com/qima/o2o/util/CodeUtil.java
+++ src/main/java/com/qima/o2o/util/CodeUtil.java
@@ -1,6 +1,16 @@
 package com.qima.o2o.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class CodeUtil {
 	public static boolean checkVerifyCode(HttpServletRequest request) {
@@ -13,5 +23,27 @@ public class CodeUtil {
 			return false;
 		}
 		return true;
+	}
+	
+	//生成二维码
+	public static BitMatrix generateQRCodeStream(String content,HttpServletResponse resp) {
+		resp.setHeader("Cache-Control", "no-store");
+		resp.setHeader("Pragma", "no-cache");
+		resp.setDateHeader("Expires", 0);
+		resp.setContentType("image/png");
+		
+		Map<EncodeHintType,Object> hints = new HashMap<EncodeHintType,Object>();
+		hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+		hints.put(EncodeHintType.MARGIN, 0);
+		
+		BitMatrix bitMatrix;
+		try {
+			bitMatrix=new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 300, 300,hints);
+		} catch (WriterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return bitMatrix;
 	}
 }
