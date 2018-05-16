8f4367384f6d0bcb4e14a930aa018ec2d0dffc1a
qimainfo <m18721632613_1@163.com>
2018-05-17 04:19:07 +0800

    init

--- src/main/java/com/qima/o2o/O2oApplication.java
+++ src/main/java/com/qima/o2o/O2oApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class O2oApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(O2oApplication.class, args);
 	}
 }
